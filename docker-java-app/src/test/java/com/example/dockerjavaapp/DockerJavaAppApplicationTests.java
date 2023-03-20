39581cb0fe3d71bda28afa665372dd57aa63b7f4
trystenb <trym.stenberg@inmeta.no>
2023-03-20 13:55:19 +0100

    Initial commit

---
+++ docker-java-app/src/test/java/com/example/dockerjavaapp/DockerJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
